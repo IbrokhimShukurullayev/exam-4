ba76e14439846ac385274b43cd1666d3840ecf1a
IbrokhimShukurullayev <ibrokhimjonshukurullayev>
2024-01-24 18:20:58 +0500

    ibrohim

--- js/main.js
+++ js/main.js
@@ -19,10 +19,39 @@ main.addEventListener("click", () => {
 let count = 0;
 const btn = document.querySelectorAll(".card-link");
 const span = document.querySelector(".div-item-text");
+// if (count == 0) {
+//   span.style.display = "none"
+// }
 
 for (let i = 0; i < btn.length; i++) {
   btn[i].addEventListener("click", () => {
     count++;
+    // span.style.display = "inline-block"; 
     span.innerHTML = count;
   });
 }
+
+const buttonLinks = document.querySelector(".card-link");
+const plusMinus = document.querySelector(".card__plus-minus");
+const minus = document.querySelector(".minus");
+const cardCantent = document.querySelector(".card__cantent");
+const plus = document.querySelector(".plus");
+
+let countCard = 0;
+
+buttonLinks.addEventListener("click", function () {
+  this.style.display = "none";
+  plusMinus.style.display = "flex";
+  countCard++;
+  cardCantent.innerHTML = countCard;
+});
+
+plus.addEventListener("click", function () {
+  countCard++;
+  cardCantent.innerHTML = countCard;
+});
+
+minus.addEventListener("click", function () {
+  countCard--;
+  cardCantent.innerHTML = countCard;
+});
